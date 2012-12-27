2a0e926d13a5cdf08e569388e51bc06b61cf0c30
Doug Headley <headley.douglas@gmail.com>
2012-12-27 02:11:15 -0800

    Carrierwave is the choice for uploading. #S3 will be a must if using heroku.

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,10 @@
 class Message < ActiveRecord::Base
-  attr_accessible :audo, :image
+  attr_accessible :audio, :image
   belongs_to :card
-  mount_uploader :audo, AudioUploader
+
+  # mount_uploader :audio, AudioUploader
+  mount_uploader :image, ImageUploader
+  mount_uploader :audio, AudioUploader
+
+  validates_presence_of :audio, :image
 end
