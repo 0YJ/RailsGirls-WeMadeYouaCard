8175c222dbdc48fe33966ba63c3529a1e288484f
Doug Headley <headley.douglas@gmail.com>
2012-12-31 11:55:41 -0800

    Messages controller gets messages for given cards.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,7 +2,7 @@ class MessagesController < ApplicationController
   # GET /messages
   # GET /messages.json
   def index
-    @messages = Message.all
+    @messages = Card.find(params[:card_id]).messages
 
     respond_to do |format|
       format.html # index.html.erb
